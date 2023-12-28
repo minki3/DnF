06268132e657d976a5fa4adb918d8f55b1069fa7
minki3 <86992006+minki3@users.noreply.github.com>
2023-12-29 01:58:34 +0900

    Feat:닉네임 정보 리덕스 연결

--- src/components/CharacterSearch.tsx
+++ src/components/CharacterSearch.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { getServerApi } from '@/service/api/getServerApi'
 import ServerSelectBox from '@/components/ServerSelectBox'
+import NickNameInput from '@/components/NickNameInput'
 
 export default async function CharacterSearch() {
   const getServerData = await getServerApi()
@@ -8,6 +9,7 @@ export default async function CharacterSearch() {
   return (
     <div>
       <ServerSelectBox serverData={getServerData} />
+      <NickNameInput />
     </div>
   )
 }
--- src/components/ServerSelectBox.tsx
+++ src/components/ServerSelectBox.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { ServerDataType } from '@/service/types/type'
 import { useAppDispatch, useAppSelector } from '@/lib/redux/hooks'
 import { serverChange } from '@/lib/redux/features/characterServerState'
@@ -9,28 +9,33 @@ interface Props {
 }
 
 export default function ServerSelectBox({ serverData }: Props) {
-  const server = useAppSelector((state) => state.characterServerState.value)
+  const [isOpen, setIsOpen] = useState(false)
   const dispatch = useAppDispatch()
+
+  const userData = useAppSelector((state) => state.characterServerState.value)
 
   const serverHandler = (server: string) => {
     dispatch(serverChange(server))
   }
-
+  console.log(userData)
   return (
-    <ul>
-      {serverData.rows.map((item) => {
-        const { serverId, serverName } = item
-        return (
-          <li
-            key={serverId}
-            onClick={() => {
-              serverHandler(serverId)
-            }}
-          >
-            {serverName}
-          </li>
-        )
-      })}
-    </ul>
+    <>
+      <ul className="py-2">
+        {serverData.rows.map((item) => {
+          const { serverId, serverName } = item
+          return (
+            <li
+              key={serverId}
+              onClick={() => {
+                serverHandler(serverId)
+              }}
+              className="text-sm hover:cursor-pointer"
+            >
+              {serverName}
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
