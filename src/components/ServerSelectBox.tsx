31988a494643b61f604bdd130711f00662f203e8
minki3 <86992006+minki3@users.noreply.github.com>
2023-12-28 20:58:32 +0900

    Feat: 서버정보 redux를 통한 상태관리

--- src/components/ServerSelectBox.tsx
+++ src/components/ServerSelectBox.tsx
@@ -1,16 +1,35 @@
+'use client'
 import React from 'react'
 import { ServerDataType } from '@/service/types/type'
+import { useAppDispatch, useAppSelector } from '@/lib/redux/hooks'
+import { serverChange } from '@/lib/redux/features/characterServerState'
 
 interface Props {
   serverData: ServerDataType
 }
 
 export default function ServerSelectBox({ serverData }: Props) {
+  const server = useAppSelector((state) => state.characterServerState.value)
+  const dispatch = useAppDispatch()
+
+  const serverHandler = (server: string) => {
+    dispatch(serverChange(server))
+  }
+
   return (
     <ul>
       {serverData.rows.map((item) => {
         const { serverId, serverName } = item
-        return <li key={serverId}>{serverName}</li>
+        return (
+          <li
+            key={serverId}
+            onClick={() => {
+              serverHandler(serverId)
+            }}
+          >
+            {serverName}
+          </li>
+        )
       })}
     </ul>
   )
