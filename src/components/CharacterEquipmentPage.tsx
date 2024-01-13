d004e85ac951170fbe65785eae208debf485cd4f
minki3 <86992006+minki3@users.noreply.github.com>
2024-01-13 23:45:55 +0900

    Feat:장비강화 수치 적용

--- src/components/CharacterEquipmentPage.tsx
+++ src/components/CharacterEquipmentPage.tsx
@@ -1,19 +1,23 @@
 import { EquipmentType } from '@/service/types/type'
 import { rarityColor } from '@/service/utils/rarityColor'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 interface Props {
   characterEquipment: EquipmentType[]
 }
 
 export default function CharacterEquipmentPage({ characterEquipment }: Props) {
-  console.log(characterEquipment)
+  const [open, setIsOpen] = useState(false)
+  const contaionRef: any = useRef()
+  console.log(open)
 
+  const handleState = () => {
+    setIsOpen((prev) => !prev)
+  }
   return (
     <div className="flex justify-center cursor-default">
       <ul className=" basis-4/5 ">
         {characterEquipment.map((item, idx) => {
-          console.log(item)
           const {
             itemName,
             enchant,
@@ -22,20 +26,20 @@ export default function CharacterEquipmentPage({ characterEquipment }: Props) {
             reinforce,
             amplificationName,
             slotName,
+            fixedOption,
           } = item
           return (
             <li key={idx} className="p-6 border flex items-center">
-              <span className=" basis-1/3">{slotName}</span>
-              <div className="flex flex-col basis-1/3">
-                <span
-                  className={`${rarityColor(itemRarity)}`}
-                  onMouseEnter={() => {
-                    console.log('over')
-                  }}
-                  // onMouseLeave={() => {
-                  //   console.log('leave')
-                  // }}
-                >
+              <span className=" basis-1/4">{slotName}</span>
+              <div
+                className="flex flex-col basis-1/4 "
+                ref={(e) => {
+                  contaionRef
+                }}
+                onMouseLeave={handleState}
+              >
+                <div onMouseEnter={handleState} />
+                <span className={`${rarityColor(itemRarity)}`}>
                   {reinforce !== 0 && (
                     <span
                       className={`${
@@ -48,18 +52,24 @@ export default function CharacterEquipmentPage({ characterEquipment }: Props) {
                     </span>
                   )}
                   &nbsp;{itemName}
+                  {open && <div>open</div>}
                 </span>
                 <span className="text-[12px]">{skin && skin.itemName}</span>
               </div>
-              <div className=" basis-1/3 flex flex-col text-[12px] text-end">
-                {enchant.status.map((item, idx) => {
-                  const { name, value } = item
-                  return (
-                    <span key={idx}>
-                      {name} : {value}
-                    </span>
-                  )
-                })}
+              <div className="basis-1/4 text-end">
+                {fixedOption && <span>Lv {fixedOption.level}</span>}
+              </div>
+              <div className=" basis-1/4 flex flex-col text-[12px] text-end">
+                {enchant &&
+                  enchant.status &&
+                  enchant.status.map((item, idx) => {
+                    const { name, value } = item
+                    return (
+                      <span key={idx}>
+                        {name} : {value}
+                      </span>
+                    )
+                  })}
               </div>
             </li>
           )
