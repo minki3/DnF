e1d17759449bfe4b75d7ea1ad04c112cbbde4953
minki3 <86992006+minki3@users.noreply.github.com>
2024-01-26 03:09:34 +0900

    Feat:랭커 클릭시 상세페이지 이동

--- src/components/CharacterRankBox.tsx
+++ src/components/CharacterRankBox.tsx
@@ -1,5 +1,6 @@
 import { CharacterInformationType } from '@/service/types/type'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 interface Props {
@@ -22,32 +23,39 @@ export default function CharacterRankBox({ characterData, idx, page }: Props) {
   } = characterData
 
   return (
-    <div className="p-6 flex items-center border w-full">
-      <div className="flex items-center pl-4">
-        {/* <span>{page === 1 ? idx + 1 : idx + 1 + page * 10}위</span> */}
+    <Link
+      href={{
+        pathname: '/character',
+        query: { server: serverId, Id: characterId },
+      }}
+      className='className="p-4 flex items-center border w-full m-6'
+    >
+      <div className="flex items-center pl-4 basis-1/4">
         {page === 1 ? (
-          <span>{idx + 1}</span>
+          <span>{idx + 1}위</span>
         ) : page === 2 ? (
-          <span>{idx + 1 + 10}</span>
+          <span>{idx + 1 + 10}위</span>
         ) : (
-          <span>{idx + 1 + (page - 1) * 10}</span>
+          <span>{idx + 1 + (page - 1) * 10}위</span>
         )}
 
         <Image
           src={`https://img-api.neople.co.kr/df/servers/${serverId}/characters/${characterId}`}
           alt="character"
           width={250}
-          height={200}
+          height={250}
         />
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col basis-1/4">
         <span>{characterName}</span>
         <span>{jobGrowName}</span>
       </div>
-      <div>{/* <span>Lv : {level}</span> */}</div>
-      <div>
+      <div className="basis-1/4">
+        <span>Lv : {level}</span>
+      </div>
+      <div className="basis-1/4 text-center">
         <span>{fame}</span>
       </div>
-    </div>
+    </Link>
   )
 }
