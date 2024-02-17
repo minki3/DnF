25f8836d5ca5cbc6cbded39eeca8049a1020e3e3
minki3 <86992006+minki3@users.noreply.github.com>
2024-02-17 19:31:06 +0900

    Feat:버프랭킹페이지, 딜러랭킹페이지 컴포넌트 병합

--- src/components/JobRankPage.tsx
+++ src/components/JobRankPage.tsx
@@ -2,12 +2,20 @@ import React, { Suspense, useEffect, useState } from 'react'
 import { getCharacterFameRankCsr } from '@/service/api/getCharacterFameRank'
 import { CharacterRankType } from '@/service/types/type'
 import PagenationArea from '@/components/PagenationArea'
-import { category } from '@/service/utils/JobCategory'
 import RankCategory from '@/components/RankCategory'
 import { motion } from 'framer-motion'
 import SkeletonComponent from '@/components/skeleton/SkeletonComponent'
 
-export default function AttackerRankPage() {
+interface Props {
+  isBuff: boolean
+  category: {
+    job: string
+    jobId: string
+    growJob: { job: string; jobId: string }[]
+  }[]
+}
+
+export default function AttackerRankPage({ isBuff, category }: Props) {
   const CharacterRankBox = React.lazy(
     () => import('@/components/CharacterRankBox'),
   )
@@ -31,7 +39,7 @@ export default function AttackerRankPage() {
   useEffect(() => {
     getCharacterFameRankCsr(
       'all',
-      false,
+      isBuff,
       job.jobId,
       job.growJobId,
       null,
