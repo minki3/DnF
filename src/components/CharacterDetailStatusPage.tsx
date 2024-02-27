94931fcbd54e97f71938d75f08707bae77336452
minki3 <86992006+minki3@users.noreply.github.com>
2024-02-27 11:12:59 +0900

    refactor:input창 한글입력 후 엔터를 누르면 두번 검색되어지는 오류 해결

--- src/components/CharacterDetailStatusPage.tsx
+++ src/components/CharacterDetailStatusPage.tsx
@@ -24,7 +24,10 @@ export default function CharacterStatusPage({ characterStatus }: Props) {
         {characterStatus &&
           filterData.map((item, idx) => {
             return (
-              <li className="text-[8px] md:text-[13px] lg:text-base p-2 lg:p-6 border flex items-center justify-between hover:cursor-default">
+              <li
+                key={idx}
+                className="text-[8px] md:text-[13px] lg:text-base p-2 lg:p-6 border flex items-center justify-between hover:cursor-default"
+              >
                 <span className="">{item.name}</span>
                 <span className="font-bold">{item.value}</span>
               </li>
